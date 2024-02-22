625f833ddba83a3a626e35f944a1688ddde34988
Mallikarjuna29429 <kc_malli@rediffmail.co.in>
2024-02-22 17:23:45 +0530

    fetch & axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    };
+    getPosts();
+    //resuts.then((error) => console.log("error occured"));
+    //if (error) return <Error error={error} />;
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Post Data</p>
+      {loading ? (
+        <h4 style={{ color: "red" }}>Loading data..wait</h4>
+      ) : (
+        posts.map((post) => {
+          return <h4 key={post.id}>{post.title}</h4>;
+        })
+      )}
     </div>
   );
 }
